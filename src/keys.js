bfb77085e93635bb1c10e039686abbbe9b84c1d0
FabricioWlodeck <wlodeckfabricio@gmail.com>
2025-01-15 11:17:33 -0300

    agrego archivo .env, estandar para manejo de configuraciones sensibles

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,21 @@
+const dotenv = require("dotenv");
+const path = require("path");
+
+const envPath = path.join(__dirname, ".env");
+const configResult = dotenv.config({ path: envPath });
+
+
+if (configResult.error) {
+    console.error(`Failed to load environment variables. Ensure the .env file exists at ${envPath}.`);
+    console.error(configResult.error.message);
+    process.exit(1); // Detiene la ejecución si hay un error crítico
+}
+
 module.exports = {
     database: {
-        host: "localhost",
-        user: "root",
-        password:"",
-        database: "database_links"
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DB_NAME || 'default_db'
     }
 };
